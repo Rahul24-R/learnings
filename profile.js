ecded9a1f5917cfa3886076f6f2c0cb28858f854
Rahul24-R <63769743+Rahul24-R@users.noreply.github.com>
2021-08-13 11:42:11 +0530

    html_edit0.7

---
+++ profile.js
@@ -0,0 +1,26 @@
+
+var name=document.getElementById('name').value;
+function hello()
+{
+   
+   const message=`Hello ${name} thankyou for visting.
+   Your feedback has been logged !! `;
+   alert(message);
+   return false;
+}
+function leavepage()
+{
+  alert(`Hey  you are moving out to an external site.`);
+}
+ function currentlydisabled()
+{
+  alert("Sorry!! This action has been disabled due to privacy concerns!!");
+}
+
+function viewpdf(a)
+{
+  if(a===1)
+  {
+    document.getElementById("test_cert").src="file:///F:/projects/trial/certificates/htmlcss_1.pdf#toolbar=0";
+  }
+}
